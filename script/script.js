1c3dd92b416c3b5ecd26f2f0141b75f7765d5483
irunguliz <liz.irungu@student.moringaschool.com>
2021-07-25 17:05:17 +0300

    index.html

---
+++ script/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+$('#design').toogle();
+});
+
+$(document).ready(function(){
+    $('#development').toogle();
+    });
+
+    $(document).ready(function(){
+        $('#product').toogle();
+        });
