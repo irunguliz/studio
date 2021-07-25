f7f23dae4c48412849b040c465cdbdd60b95230f
irunguliz <liz.irungu@student.moringaschool.com>
2021-07-25 17:21:25 +0300

    script.js

--- script/script.js
+++ script/script.js
@@ -1,11 +1,18 @@
-$(document).ready(function(){
-$('#design').toggle();
+$(document).ready(function () {
+    $('#design').on('click', function () {
+        $('#design').show();
+    });
 });
 
-$(document).ready(function(){
-    $('#development').toggle();
-    });
 
-    $(document).ready(function(){
-        $('#product').toggle();
-        });
+$(document).ready(function () {
+    $('#development').on('click', function () {
+        $('#development').show();
+    });
+});
+
+$(document).ready(function () {
+    $('#product').on('click', function () {
+        $('#product').show();
+    });
+});
