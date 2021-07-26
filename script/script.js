f1af4818b3c8d22a85e39c5774c289bafb50cafc
irunguliz <liz.irungu@student.moringaschool.com>
2021-07-26 10:07:45 +0300

    script.js

--- script/script.js
+++ script/script.js
@@ -15,3 +15,15 @@ $(document).ready(function() {
        $("#development").toggle();
     });
 });
+
+function formValidation() {
+    let name=document.forms["form"]["name"].value;
+    let email=document.forms["form"]["email"].value;
+    let message=document.forms["form"]["message"].value;
+
+    if(name=="" || email=="" || message==""){
+        alert("Fill all fields");
+    }else{
+        alert(`name:${name}\n email address: ${email}\n We thank you for reaching out to Delani Studios. Your message has been received. We will get back to you, be sure to reach out any time. \n received by Liz Irungu`);
+    }
+}
